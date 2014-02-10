90bc7caa6b8f9667a0ca27786bdad1e12a18a6cc
Jocelyn Turcotte <jocelyn.turcotte@digia.com>
2014-02-10 13:40:17 +0100

    Initial version working with the test server

---
+++ open_code_browser.py
@@ -0,0 +1,23 @@
+import sublime, sublime_plugin
+import http.client
+import webbrowser
+
+class OpenCodeBrowser(sublime_plugin.WindowCommand):
+
+    def run(self):
+        orig_sel = None
+        v = self.window.active_view()
+        if v:
+            orig_sel = [r for r in v.sel()]
+
+        pt = v.sel()[0]
+        symbol = v.substr(v.word(pt))
+
+        c = http.client.HTTPConnection("acid.woboq.im:8001")
+        c.request("GET", "/api/fn/" + symbol)
+        entries = sublime.decode_value(c.getresponse().read().decode())
+        c.close()
+
+        self.window.show_quick_panel(
+            [e['name'] for e in entries],
+            lambda x: webbrowser.open(entries[x]['url']) if x > -1 else sublime.status_message("Unable to find " + symbol))
